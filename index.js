ce22c88e9e25044f5874de6f420b0c97cf78e777
Sheika Abuthair <105582784+Abuthair97@users.noreply.github.com>
2024-03-31 22:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    saveEl.innerText += count +"-";
+}
+
+
